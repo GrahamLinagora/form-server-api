ea1f4c5731655de8dc36ef302fd7d53811a6dfd6
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-07-29 15:51:05 +0200

    Setting form as ObjectId

--- app/models/instance.js
+++ app/models/instance.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose'), Schema = mongoose.Schema;
 var InstanceSchema = new Schema({
   name: String,
   description: String,
-  form_id: String,
+  form: {type : Schema.ObjectId, ref : 'Form'},
   published_on: { type : Date, default: Date.now },
   open: { type : Boolean, default: Boolean.true }
   },
@@ -22,6 +22,45 @@ InstanceSchema.pre('save', function(next) {
   console.log('Saving Form Instance', this);
   next();
 });
+
+/**
+ * Statics
+ *
+ * @type {{load: Function, list: Function}}
+ */
+InstanceSchema.statics = {
+
+  /**
+   * Find instance by id and populate form
+   *
+   * @param {ObjectId} id
+   * @param {Function} cb
+   * @api private
+   */
+
+  load: function (id, cb) {
+    this.findOne({ _id : id })
+      .populate('form')
+      .exec(cb)
+  },
+
+  /**
+   * Get a list of instance based on criteria
+   *
+   * @param options
+   * @param cb
+   */
+  list: function (options, cb) {
+    var criteria = options.criteria || {}
+
+    this.find(criteria)
+      .populate('form')
+      .sort({'published_on': -1}) // sort by date
+      .limit(options.perPage)
+      .skip(options.perPage * options.page)
+      .exec(cb)
+  }
+}
 
 InstanceSchema.plugin(require('mongoose-lifecycle'));
 module.exports = mongoose.model('Instance', InstanceSchema);
